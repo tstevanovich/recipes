f618fa3d2dc8b8e889b6ac024bfe1d1bc956874d
Tony Stevanovich <tstevanovich@zenmonics.com>
2018-07-13 15:52:39 -0400

    add theme, interface

--- src/app/features/recipes/recipe-list/recipe-list.component.ts
+++ src/app/features/recipes/recipe-list/recipe-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes = [];
+
   constructor() {}
 
   ngOnInit() {}
